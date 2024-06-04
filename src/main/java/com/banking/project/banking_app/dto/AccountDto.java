6d5c0374b3d9dfd107480934d58b8f02e222ba11
Murilo Nunes <murilosouza.nunes@gmail.com>
2024-06-04 09:10:20 -0300

    Testing the use of Record Class in the DTO class, to test encapsulation and data transfer between application layers

--- src/main/java/com/banking/project/banking_app/dto/AccountDto.java
+++ src/main/java/com/banking/project/banking_app/dto/AccountDto.java
@@ -1,13 +1,17 @@
 package com.banking.project.banking_app.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class AccountDto {
-
-    private Long id;
-    private String accountHolderName;
-    private double balance;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class AccountDto {
+//
+//    private Long id;
+//    private String accountHolderName;
+//    private double balance;
+//}
+public record AccountDto(Long id,
+                         String accountHolderName,
+                         double balance) {
 }
