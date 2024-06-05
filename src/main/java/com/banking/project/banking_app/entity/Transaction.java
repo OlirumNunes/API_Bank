9ff8feea837e38ec693feeac4c38074950130ecc
Murilo Nunes <murilosouza.nunes@gmail.com>
2024-06-05 11:15:40 -0300

    Creation of the Transaction class to store the transaction history + creation of the database table

---
+++ src/main/java/com/banking/project/banking_app/entity/Transaction.java
@@ -0,0 +1,28 @@
+package com.banking.project.banking_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long accountId;
+    private double amount;
+    private String transactionType; //Deposit, Withdraw, Transfer
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/com/banking/project/banking_app/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.banking.project.banking_app.repository;
+
+import com.banking.project.banking_app.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountIdOrderByTimestampDesc(Long accountId);
+}
